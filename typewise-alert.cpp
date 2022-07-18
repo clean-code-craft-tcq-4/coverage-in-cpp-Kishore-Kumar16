b120862ef8d28018fc093485541a1eef3bd493b7
Kishore <Kishorekumar161197@gmail.com>
2022-07-19 01:22:26 +0530

    BatteryTemparature status refactoring

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,18 +1,27 @@
+#define ENABLE_BREACH_STATUS
+
 #include "typewise-alert.h"
-#include <stdio.h>
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
+
+  BreachType retBreachType = NORMAL;
   if(value < lowerLimit) {
-    return TOO_LOW;
+    retBreachType = TOO_LOW;
   }
   if(value > upperLimit) {
-    return TOO_HIGH;
+    retBreachType = TOO_HIGH;
   }
-  return NORMAL;
+  return retBreachType;
 }
 
-BreachType classifyTemperatureBreach(
-    CoolingType coolingType, double temperatureInC) {
+void classifyTemperatureBreach(std::unordered_map <int, std::pair<int, int>> &tempBreach) {
+
+      //std::unordered_map <std::string, std::pair<int , int> > tempBreach;
+      tempBreach.insert({PASSIVE_COOLING, std::make_pair<int, int>(0, 35)});
+      tempBreach.insert({HI_ACTIVE_COOLING, std::make_pair<int, int>(0, 45)});
+      tempBreach.insert({MED_ACTIVE_COOLING, std::make_pair<int, int>(0, 40)});
+      
+/*
   int lowerLimit = 0;
   int upperLimit = 0;
   switch(coolingType) {
@@ -29,16 +38,24 @@ BreachType classifyTemperatureBreach(
       upperLimit = 40;
       break;
   }
+
   return inferBreach(temperatureInC, lowerLimit, upperLimit);
+  */
 }
 
 void checkAndAlert(
     AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
 
-  BreachType breachType = classifyTemperatureBreach(
-    batteryChar.coolingType, temperatureInC
-  );
+    std::unordered_map <int, std::pair<int, int>> temp_Breach;
+  classifyTemperatureBreach(temp_Breach);
 
+  int lowerLimit, upperLimit;
+  auto itr = temp_Breach.find(batteryChar.coolingType);
+  lowerLimit = itr->second.first;
+  upperLimit = itr->second.second;
+  BreachType breachType = inferBreach(temperatureInC, lowerLimit, upperLimit);
+  sendBatteryTempStatus(alertTarget, breachType);
+/*
   switch(alertTarget) {
     case TO_CONTROLLER:
       sendToController(breachType);
@@ -47,8 +64,28 @@ void checkAndAlert(
       sendToEmail(breachType);
       break;
   }
+  */
 }
 
+void sendBatteryTempStatus(AlertTarget alertTarget, BreachType breachType, std::ostream& os, const char* recepientId) {
+
+  if (alertTarget == TO_EMAIL) {
+    os << "To: " << recepientId << std::endl;
+    os << "Hi, the temperature is " << breachStatus[breachType] << std::endl;
+    /*
+    printf("To: %s\n", recepientId);
+    printf("Hi, the temperature is %s", breachStatus[breachType]);
+    */
+  }
+  else {
+    const unsigned short header = 0xfeed;
+    os << header << " : " << breachType << std::endl;
+    
+    //printf("%x : %x\n", header, breachType);
+  }
+}
+
+/*
 void sendToController(BreachType breachType) {
   const unsigned short header = 0xfeed;
   printf("%x : %x\n", header, breachType);
@@ -69,3 +106,4 @@ void sendToEmail(BreachType breachType) {
       break;
   }
 }
+*/
